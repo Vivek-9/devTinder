23a322b2b345b2715b4b6164499b1d07a9ece454
Vivek <vivek.vanga@gmail.com>
2025-06-15 15:16:52 +0530

    sample http methods

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 
 
+app.use("/home/2",(req,res) => { 
+    res.send('This is Second Home page');  
+})
+
 
 app.use("/home",(req,res) => { 
     res.send('This is Home page');  
@@ -12,6 +16,17 @@ app.use('/about',(req,res) => {
     res.send('This is about page...');
 })
 
+app.get('/user', (req,res) => {
+    res.send({firstName:"Vivek", lastName:"Vanga"})
+})
+
+app.post('/user', (req,res) => {
+    res.send('Data successfully saved to Database');
+})
+
+app.delete('/user', (req,res) => {
+    res.send('Deleted Successfully');
+})
 app.use("/",(req, res) => {
     res.send("Hello from the server!");
 })
